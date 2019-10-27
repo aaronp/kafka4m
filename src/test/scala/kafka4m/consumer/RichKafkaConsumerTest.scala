1c03913288f8c91a2290e973ac0a2836871acc89
Aaron Pritzlaff <aaron.pritzlaff@gmail.com>
2019-10-27 19:58:49 +0000

    closing producers/consumers in tests

--- src/test/scala/kafka4m/consumer/RichKafkaConsumerTest.scala
+++ src/test/scala/kafka4m/consumer/RichKafkaConsumerTest.scala
@@ -6,9 +6,10 @@ import com.typesafe.config.{Config, ConfigFactory}
 import kafka4m.BaseKafka4mDockerSpec
 import kafka4m.admin.RichKafkaAdmin
 import kafka4m.producer.RichKafkaProducer
-import kafka4m.util.Schedulers
+import kafka4m.util.{Schedulers, Using}
 
 class RichKafkaConsumerTest extends BaseKafka4mDockerSpec {
+
   import RichKafkaConsumerTest.testConfig
 
   private val id = new AtomicLong(System.currentTimeMillis)
@@ -20,28 +21,28 @@ class RichKafkaConsumerTest extends BaseKafka4mDockerSpec {
   "RichKafkaConsumer.status" should {
     "report current assignments and partitions" in {
       Schedulers.using { implicit sched =>
-        val topic                                            = nextTopic()
-        val config                                           = testConfig(topic)
-        val consumer: RichKafkaConsumer[String, Array[Byte]] = RichKafkaConsumer.byteArrayValues(config)
+        val topic  = nextTopic()
+        val config = testConfig(topic)
+        Using(RichKafkaConsumer.byteArrayValues(config)) { consumer =>
+          Using(RichKafkaAdmin(config))(_.createTopicSync(topic, testTimeout))
 
-        RichKafkaAdmin(config).createTopicSync(topic, testTimeout)
-
-        val statusLines = consumer.status(true).linesIterator.toList
-        statusLines should contain("currently assigned to 0: []")
-        statusLines.map(_.trim) should contain(s"Partition(topic = $topic, partition = 0, leader = 0, replicas = [0], isr = [0], offlineReplicas = [])")
+          val statusLines = consumer.status(true).linesIterator.toList
+          statusLines should contain("currently assigned to 0: []")
+          statusLines.map(_.trim) should contain(s"Partition(topic = $topic, partition = 0, leader = 0, replicas = [0], isr = [0], offlineReplicas = [])")
+        }
       }
     }
   }
   "RichKafkaConsumer.assignmentPartitions" should {
     "return the assignmentPartitions" in {
       Schedulers.using { implicit sched =>
-        val topic                                            = nextTopic()
-        val config                                           = testConfig(topic)
-        val consumer: RichKafkaConsumer[String, Array[Byte]] = RichKafkaConsumer.byteArrayValues(config)
+        val topic  = nextTopic()
+        val config = testConfig(topic)
+        Using(RichKafkaConsumer.byteArrayValues(config)) { consumer =>
+          Using(RichKafkaAdmin(config))(_.createTopicSync(topic, testTimeout))
 
-        RichKafkaAdmin(config).createTopicSync(topic, testTimeout)
-
-        consumer.assignmentPartitions shouldBe empty
+          consumer.assignmentPartitions shouldBe empty
+        }
       }
     }
   }
@@ -49,33 +50,34 @@ class RichKafkaConsumerTest extends BaseKafka4mDockerSpec {
     "seek to the beginning" in {
       Schedulers.using { implicit sched =>
         Given("Some messages in a topic")
-        val topic                                            = nextTopic()
-        val config                                           = testConfig(topic)
-        val consumer: RichKafkaConsumer[String, Array[Byte]] = RichKafkaConsumer.byteArrayValues(config)
+        val topic  = nextTopic()
+        val config = testConfig(topic)
+        Using(RichKafkaConsumer.byteArrayValues(config)) { consumer =>
+          Using(RichKafkaProducer.byteArrayValues(config)) { producer =>
+            val first  = producer.sendAsync(topic, "first", "value".getBytes(), partition = 0).futureValue
+            val second = producer.sendAsync(topic, "second", "value".getBytes(), partition = 0).futureValue
+            val third  = producer.sendAsync(topic, "third", "value".getBytes(), partition = 0).futureValue
 
-        val producer = RichKafkaProducer.byteArrayValues(config)
-        val first    = producer.sendAsync(topic, "first", "value".getBytes(), partition = 0).futureValue
-        val second   = producer.sendAsync(topic, "second", "value".getBytes(), partition = 0).futureValue
-        val third    = producer.sendAsync(topic, "third", "value".getBytes(), partition = 0).futureValue
+            When("We subscribe and consume to the end")
+            consumer.subscribe(topic, RebalanceListener)
 
-        When("We subscribe and consume to the end")
-        consumer.subscribe(topic, RebalanceListener)
+            eventually {
+              consumer.poll().toList.size shouldBe 3
+            }
 
-        eventually {
-          consumer.poll().toList.size shouldBe 3
+            And("seek to the beginning")
+            eventually {
+              consumer.seekToBeginning(0) shouldBe true
+            }
+
+            Then("we should see that offset as the first message")
+            val readBack = eventually {
+              consumer.poll().toList.head
+            }
+            readBack.key() shouldBe "first"
+            readBack.offset() shouldBe first.offset()
+          }
         }
-
-        And("seek to the beginning")
-        eventually {
-          consumer.seekToBeginning(0) shouldBe true
-        }
-
-        Then("we should see that offset as the first message")
-        val readBack = eventually {
-          consumer.poll().toList.head
-        }
-        readBack.key() shouldBe "first"
-        readBack.offset() shouldBe first.offset()
       }
     }
   }
@@ -83,12 +85,12 @@ class RichKafkaConsumerTest extends BaseKafka4mDockerSpec {
 
 object RichKafkaConsumerTest {
   def testConfig(topic: String): Config = ConfigFactory.parseString(s"""kafka4m.admin.topic : $topic
-                                                                       |
-                                                                       |kafka4m.consumer.topic : $topic
-                                                                       |kafka4m.consumer.group.id : "test"$topic
-                                                                       |kafka4m.consumer.application.id : "test"$topic
-                                                                       |kafka4m.consumer.auto.offset.reset : earliest
-                                                                       |
-                                                                       |kafka4m.producer.topic : $topic
-                                                                       |""".stripMargin).withFallback(ConfigFactory.load())
+       |
+       |kafka4m.consumer.topic : $topic
+       |kafka4m.consumer.group.id : "test"$topic
+       |kafka4m.consumer.application.id : "test"$topic
+       |kafka4m.consumer.auto.offset.reset : earliest
+       |
+       |kafka4m.producer.topic : $topic
+       |""".stripMargin).withFallback(ConfigFactory.load())
 }
